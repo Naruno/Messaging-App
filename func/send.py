f7e66516fdb4052fc6f0b9ebf84e4e5d75a8d412
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-06-08 14:06:57 +0300

    Update send.py

--- func/send.py
+++ func/send.py
@@ -12,10 +12,10 @@ def add_new_user_request(publickey):
     key = the_keys()["1"]
 
     data = {"app":"messagingapp", "command":"addnewuser", "n":key["n"], "e":key["e"]}
-    send("123", publickey, amount = 0, data = data)
+    send("123", publickey, amount = 1000, data = data)
 
     create_new_user("unknow",publickey,0,0)
 
 def send_new_message(message,publickey):
     data = {"app":"messagingapp", "command":"newmessage", "message":message}
-    send("123", publickey, amount = 0, data = data)
+    send("123", publickey, amount = 1000, data = data)
