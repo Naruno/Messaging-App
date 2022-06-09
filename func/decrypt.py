9ef83ce0be5f92a63d8f97bb2f4290f9458ceaba
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-06-10 01:08:30 +0300

    Update decrypt.py

--- func/decrypt.py
+++ func/decrypt.py
@@ -2,7 +2,7 @@ from app.Messaging_App.lib.keys_system import the_keys, save_keys
 
 from app.Messaging_App.func.save_new_message import save_new_message
 
-from wallet.wallet import Address
+from wallet.wallet import Address, Wallet_Import
 
 def decrypt_text(text_data,pubkey):
 
@@ -29,21 +29,22 @@ def decrypt_text(text_data,pubkey):
     message_publickey = Address(my_address)
 
     d = temp_keys["1"]["d"]
-
-    for keys in temp_keys:
-      if "fromUser" in temp_keys[keys]:
-        key_publickey = temp_keys[keys]["fromUser"].replace("\n","").replace(" ","")
-        if message_publickey == key_publickey:
-            i = 0
-            decrypted_Mess = ""
-            while i < l -2:
-                x = decrypt(nums[i], int(d), int(n))
-                y = chr(x)
-                decrypted_Mess += y
-                i+=1
-            message = {"from":"user","message":decrypted_Mess}
-            save_new_message(message,temp_keys[keys]["n"],temp_keys[keys]["e"])
-            return decrypted_Mess
+    
+    if not message_publickey == Wallet_Import(-1, 3):
+        for keys in temp_keys:
+          if "fromUser" in temp_keys[keys]:
+            key_publickey = temp_keys[keys]["fromUser"].replace("\n","").replace(" ","")
+            if message_publickey == key_publickey:
+                i = 0
+                decrypted_Mess = ""
+                while i < l -2:
+                    x = decrypt(nums[i], int(d), int(n))
+                    y = chr(x)
+                    decrypted_Mess += y
+                    i+=1
+                message = {"from":"user","message":decrypted_Mess}
+                save_new_message(message,temp_keys[keys]["n"],temp_keys[keys]["e"])
+                return decrypted_Mess
 
 
 def decrypt(c,d,n):
