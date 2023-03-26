cfb1b87f2b421e1756f385d91c009459f2ca9aaa
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-03-26 23:17:38 +0300

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='messaging_app',
-version='0.1.2',
+version='0.1.3',
 description="""Messaging App""",
 long_description_content_type="text/markdown",
 long_description="".join(open("README.md", encoding="utf-8").readlines()),
