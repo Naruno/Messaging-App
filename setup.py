cf7f5f566807bf9019124fbe9121722a7f9455bf
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-04-05 09:46:14 +0300

    release: Added v0.1.8-alpha

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='messaging_app',
-version='0.1.7',
+version='0.1.8',
 description="""Messaging App""",
 long_description_content_type="text/markdown",
 long_description="".join(open("README.md", encoding="utf-8").readlines()),
